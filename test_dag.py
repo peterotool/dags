da50c90ca628cb756bc21fcdf5f9e36cd4f724c5
Jose Pedro <jcordero@lipigas.cl>
2022-03-28 16:52:41 -0300

    add dags

--- test_dag.py
+++ test_dag.py
@@ -6,6 +6,11 @@ with DAG("test_task", start_date=datetime(2021, 1, 1),
          schedule_interval="@daily", catchup=False) as dag:
 
     bash_task = BashOperator(
+        task_id="bash_task0",
+        bash_command="pwd",
+    )
+
+    bash_task = BashOperator(
         task_id="bash_task1",
         bash_command="gcloud auth activate-service-account --key-file='./datalake-data-manager.json' --project=emplipigas-datalake",
     )
